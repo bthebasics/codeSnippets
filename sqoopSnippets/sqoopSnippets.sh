9f6d2d6ca4c83b4894f32f954924c2b1af9df6a8
Abhijeet Rajput <bthebasics@gmail.com>
2018-10-08 22:01:58 -0400

    Sqoop Snippets added

---
+++ sqoopSnippets/sqoopSnippets.sh
@@ -0,0 +1,14 @@
+##https://sqoop.apache.org/docs/1.99.2/ClientAPI.html
+
+#Using SQOOP with Java ...
+#to use sqoop on java i add those jar into my java project:
+
+commons-io.jar
+commons-logging.jar
+hadoop-common.jar
+hive-jdbc.jar
+jackson-core-asl
+jackson-mapper-asl.jar
+log4j.jar
+sqoop-1.4.jar
+vertica-jdk.jar
